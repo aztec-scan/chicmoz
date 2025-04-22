442265a102be3a0a4fe884c875a5572e1373336d
filip <filip.harald@gmail.com>
2025-04-22 10:22:54 +0200

    hotfix: removed unecessary label

--- services/explorer-api/src/svcs/http-server/routes/controllers/chain-info.ts
+++ services/explorer-api/src/svcs/http-server/routes/controllers/chain-info.ts
@@ -11,7 +11,7 @@ import {
 export const openapi_GET_CHAIN_INFO: OpenAPIObject["paths"] = {
   "/l2/info": {
     get: {
-      tags: ["l2-chain", "l2-chain-info"],
+      tags: ["l2-chain"],
       summary: "Get L2 chain info",
       responses: chainInfoResponse,
     },
@@ -22,14 +22,17 @@ export const GET_CHAIN_INFO = asyncHandler(async (_req, res) => {
   const chainInfo = await dbWrapper.get(["l2", "chain-info"], () =>
     db.l2.getL2ChainInfo(L2_NETWORK_ID),
   );
-  if (!chainInfo) {res.status(404).send("Chain info not found");}
-  else {res.status(200).send(chainInfo);}
+  if (!chainInfo) {
+    res.status(404).send("Chain info not found");
+  } else {
+    res.status(200).send(chainInfo);
+  }
 });
 
 export const openapi_GET_CHAIN_ERRORS: OpenAPIObject["paths"] = {
   "/l2/errors": {
     get: {
-      tags: ["l2-chain", "l2-chain-errors"],
+      tags: ["l2-chain"],
       summary: "Get L2 chain errors",
       responses: chainErrorsResponse,
     },
@@ -41,6 +44,8 @@ export const GET_CHAIN_ERRORS = asyncHandler(async (_req, res) => {
     ["l2", "chain-errors"],
     db.l2.getL2RpcNodeErrors,
   );
-  if (!chainErrors) {throw new Error("Chain errors not found");}
+  if (!chainErrors) {
+    throw new Error("Chain errors not found");
+  }
   res.status(200).send(chainErrors);
 });
