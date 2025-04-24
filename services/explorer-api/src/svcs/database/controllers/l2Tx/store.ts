831f730ffb7eb9e74a391ca3babe0f6e99cada8f
filip <filip.harald@gmail.com>
2025-04-24 16:59:07 +0200

    fixed it

--- services/explorer-api/src/svcs/database/controllers/l2Tx/store.ts
+++ services/explorer-api/src/svcs/database/controllers/l2Tx/store.ts
@@ -1,13 +1,18 @@
-import { type ChicmozL2PendingTx } from "@chicmoz-pkg/types";
 import { getDb as db } from "@chicmoz-pkg/postgres-helper";
+import { type ChicmozL2PendingTx } from "@chicmoz-pkg/types";
+import { logger } from "../../../../logger.js";
 import { l2Tx } from "../../schema/l2tx/index.js";
 
 export const storeL2Tx = async (tx: ChicmozL2PendingTx): Promise<void> => {
-  await db()
+  const res = await db()
     .insert(l2Tx)
     .values({
       hash: tx.hash,
       birthTimestamp: new Date(tx.birthTimestamp),
     })
-    .onConflictDoNothing();
+    .onConflictDoNothing()
+    .returning();
+  if (res.length > 0) {
+    logger.info(`🕐 New pending tx: ${tx.hash}`);
+  }
 };
