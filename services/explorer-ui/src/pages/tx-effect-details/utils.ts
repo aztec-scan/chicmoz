e814ccbd5fef0effe92f7655047d194596f9deae
filip <filip.harald@gmail.com>
2025-04-25 11:11:40 +0200

    done

--- services/explorer-ui/src/pages/tx-effect-details/utils.ts
+++ services/explorer-ui/src/pages/tx-effect-details/utils.ts
@@ -1,37 +1,20 @@
 import { type ChicmozL2TxEffectDeluxe } from "@chicmoz-pkg/types";
 import { formatTimeSince } from "~/lib/utils";
 import { API_URL, aztecExplorer } from "~/service/constants";
-import { TabId } from "./types";
+import { type TabId } from "./types";
 
 export type TxEffectDataType =
   | string[][]
   | string[]
   | Array<{ logs: Array<{ data: Buffer; contractAddress: string }> }>
   | Array<{
-    logs: Array<{
-      data: Buffer;
-      maskedContractAddress: string;
-    }>;
-  }>
+      logs: Array<{
+        data: Buffer;
+        maskedContractAddress: string;
+      }>;
+    }>
   | Array<{ logs: Array<{ data: Buffer }> }>
   | Array<{ leafSlot: string; value: string }>;
-
-export const naiveDecode = (data: string[]): string => {
-  let counterZero = 0;
-  let counterAbove128 = 0;
-  const charCodes: number[] = data
-    ?.map((hex) => parseInt(hex, 16))
-    .map((charCode) => {
-      if (charCode === 0) counterZero++;
-      if (charCode > 128) counterAbove128++;
-      return charCode;
-    });
-  const isNoWeirdChars = counterZero + counterAbove128 === 0;
-  const isProbablyAReadableString =
-    isNoWeirdChars || data.length - charCodes.indexOf(0) === counterZero;
-  const res = charCodes.map((char) => String.fromCharCode(char)).join("");
-  return isProbablyAReadableString ? res : data.join("\n");
-};
 
 export const getTxEffectData = (data: ChicmozL2TxEffectDeluxe) => [
   {
@@ -64,7 +47,7 @@ export const getTxEffectData = (data: ChicmozL2TxEffectDeluxe) => [
 ];
 
 export const mapTxEffectsData = (
-  data?: ChicmozL2TxEffectDeluxe
+  data?: ChicmozL2TxEffectDeluxe,
 ): Record<TabId, boolean> => {
   return {
     privateLogs: !!data?.privateLogs?.length,
