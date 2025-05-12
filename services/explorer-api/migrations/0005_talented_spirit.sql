e799f6d0904a32da329a50327e73fcd70c27fcf6
filip <filip.harald@gmail.com>
2025-05-12 13:58:44 +0200

    done

--- services/explorer-api/migrations/0005_talented_spirit.sql
+++ services/explorer-api/migrations/0005_talented_spirit.sql
@@ -2,7 +2,6 @@
 ALTER TABLE "l1_l2_validator" ADD COLUMN "rollup_address" varchar(42);
 
 -- Update existing rows with appropriate values
--- Replace with your logic to determine the correct rollup_address for each validator
 UPDATE "l1_l2_validator" SET "rollup_address" = '0x8d1cc702453fa889f137dbd5734cdb7ee96b6ba0'; -- Current rollup address as time of writing
 
 -- Add the NOT NULL constraint
