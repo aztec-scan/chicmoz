a5bf6ddb0b1da2612157a06fcf43d6767c9698b5
filip <filip.harald@gmail.com>
2025-05-13 15:10:18 +0200

    contract types

--- services/explorer-ui/src/pages/contract-class-details/key-value-helpers.ts
+++ services/explorer-ui/src/pages/contract-class-details/key-value-helpers.ts
@@ -2,6 +2,7 @@ import {
   ContractType,
   type ChicmozL2ContractClassRegisteredEvent,
 } from "@chicmoz-pkg/types";
+import { type DetailItem } from "~/components/info-display/key-value-display";
 import { routes } from "~/routes/__root";
 import { API_URL, aztecExplorer } from "~/service/constants";
 
@@ -9,7 +10,7 @@ export const getContractClassKeyValueData = (
   data: ChicmozL2ContractClassRegisteredEvent,
   instanceAmount?: string,
 ) => {
-  return [
+  const valueData: DetailItem[] = [
     {
       label: "BLOCK HASH",
       value: data.blockHash,
@@ -40,12 +41,17 @@ export const getContractClassKeyValueData = (
       )}`,
     },
     {
-      label: "CONTRACT TYPE",
-      value: data.contractType ?? ContractType.Unknown,
-    },
-    {
       label: "INSTANCES DEPLOYED",
       value: instanceAmount,
     },
   ];
+
+  if (data.contractType && data.contractType !== ContractType.Unknown) {
+    valueData.push({
+      label: "CONTRACT TYPE",
+      value: data.contractType,
+      tooltip: "Matched with DeFi-Wonderland's standard contracts",
+    });
+  }
+  return valueData;
 };
