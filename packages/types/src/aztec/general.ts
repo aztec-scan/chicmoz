8cc239a211703eb280951e0a3a26f7daf60bcff0
filip <filip.harald@gmail.com>
2025-05-21 13:53:05 +0200

    0.87.1

--- packages/types/src/aztec/general.ts
+++ packages/types/src/aztec/general.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { l2NetworkIdSchema } from "../network-ids.js";
 
-export const CHICMOZ_TYPES_AZTEC_VERSION = "0.87.0";
+export const CHICMOZ_TYPES_AZTEC_VERSION = "0.87.1";
 
 export const L1ContractAddressesSchema = z.object({
   rollupAddress: z.string().startsWith("0x"),
