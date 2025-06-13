fa19f61e11c155be0d75eefcf25ec010258b579d
Mauro Eijsenring <mauro@eijsenring.com>
2025-06-13 15:31:04 +0200

    ui: Improve fee recipients view (#524)

--- services/explorer-ui/src/pages/fee-recipients.tsx
+++ services/explorer-ui/src/pages/fee-recipients.tsx
@@ -1,62 +1,45 @@
 import { type FC } from "react";
+import { FeeReceipientsTable } from "~/components/fee-recipient/fee-recipient-table";
+import { InfoBadge } from "~/components/info-badge";
 import { useSubTitle } from "~/hooks";
 import { useFeeRecipients } from "~/hooks/api/fee-recipient";
 import { routes } from "~/routes/__root";
 
-const tdClasses = "p-2 border border-gray-300";
-
 export const FeeRecipientPage: FC = () => {
   useSubTitle(routes.feeRecipients.title);
   const { data, isLoading, error } = useFeeRecipients();
-  return (
-    <div className="flex flex-col items-center">
-      <h1>Fee Recipients</h1>
 
-      <div className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center">
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        {data && (
-          <table>
-            <thead>
-              <tr>
-                <th>Address</th>
-                <th>Number of blocks</th>
-                <th>Total Fees Received</th>
-                <th>Part of Fees Received</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data
-                .sort(
-                  (a, b) =>
-                    (b.partOfTotalFeesReceived ?? 0) -
-                    (a.partOfTotalFeesReceived ?? 0)
-                )
-                .map((feeRecipient) => (
-                  <tr key={feeRecipient.l2Address}>
-                    <td className={`${tdClasses} font-mono`}>{feeRecipient.l2Address}</td>
-                    <td className={tdClasses}>{feeRecipient.nbrOfBlocks}</td>
-                    <td className={tdClasses}>
-                      {Number(feeRecipient.feesReceived)}
-                    </td>
-                    <td className={tdClasses}>
-                      {feeRecipient.partOfTotalFeesReceived}
-                    </td>
-                  </tr>
-                ))}
-              <tr key="total">
-                <td className={tdClasses}>TOTAL</td>
-                <td className={tdClasses}>
-                  {data.reduce((acc, v) => acc + v.nbrOfBlocks, 0)}
-                </td>
-                <td className={tdClasses}>
-                  {data.reduce((acc, v) => acc + Number(v.feesReceived), 0)}
-                </td>
-                <td className={tdClasses}>1.0</td>
-              </tr>
-            </tbody>
-          </table>
-        )}
+  const getTotalReceived = data
+    ? data.reduce((acc, cur) => {
+        return acc + Number(cur.feesReceived);
+      }, 0)
+    : 0;
+  return (
+    <div className="mx-auto px-5 max-w-[1440px] md:px-[70px]">
+      <div className="flex flex-wrap m-5">
+        <h2 className="text-primary dark:text-white mt-2 md:hidden">
+          Fee Recipients
+        </h2>
+        <h2 className="hidden md:block md:text-primary md:dark:text-white md:mt-8">
+          Fee Recipients
+        </h2>
+      </div>
+      <div className="grid grid-cols-2 gap-3 my-10 md:gap-5">
+        <InfoBadge
+          title="Amount of fee recipients"
+          isLoading={isLoading}
+          error={error}
+          data={data ? data.length.toString() : ""}
+        />
+        <InfoBadge
+          title="Total amount fees received"
+          isLoading={isLoading}
+          error={error}
+          data={getTotalReceived.toString()}
+        />
+      </div>
+      <div className="rounded-lg shadow-lg">
+        <FeeReceipientsTable feeRecipients={data} isLoading={isLoading} />
       </div>
     </div>
   );
