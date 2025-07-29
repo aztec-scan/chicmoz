0205e7ed552c760a241de79ce27985567e27634c
Mauro Eijsenring <mauro@eijsenring.com>
2025-07-29 14:53:09 +0200

    hotfix: api migration file error fix

--- services/explorer-api/migrations/0008_mysterious_sinister_six.sql
+++ services/explorer-api/migrations/0008_mysterious_sinister_six.sql
@@ -5,11 +5,11 @@ ALTER TABLE "dropped_tx" ALTER COLUMN "dropped_at" SET DATA TYPE bigint USING EX
 ALTER TABLE "tx_effect" ALTER COLUMN "tx_time_of_birth" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "tx_effect" ALTER COLUMN "tx_time_of_birth" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "tx_time_of_birth") * 1000;--> statement-breakpoint
 ALTER TABLE "tx_effect" ALTER COLUMN "tx_time_of_birth" SET DEFAULT EXTRACT(EPOCH FROM NOW()) * 1000;--> statement-breakpoint
-ALTER TABLE "l1_l2_block_proposed" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "l1_l2_proof_verified" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint;--> statement-breakpoint
+ALTER TABLE "l1_l2_block_proposed" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "l1_block_timestamp") * 1000;--> statement-breakpoint
+ALTER TABLE "l1_l2_proof_verified" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "l1_block_timestamp") * 1000;--> statement-breakpoint
 ALTER TABLE "l2Block" ALTER COLUMN "orphan_timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "orphan_timestamp") * 1000;--> statement-breakpoint
 ALTER TABLE "tx" ALTER COLUMN "birth_timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "birth_timestamp") * 1000;--> statement-breakpoint
-ALTER TABLE "l1_generic_contract_event" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint;--> statement-breakpoint
+ALTER TABLE "l1_generic_contract_event" ALTER COLUMN "l1_block_timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "l1_block_timestamp") * 1000;--> statement-breakpoint
 ALTER TABLE "l1_l2_validator_proposer" ALTER COLUMN "timestamp" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "l1_l2_validator_proposer" ALTER COLUMN "timestamp" SET DATA TYPE bigint USING EXTRACT(EPOCH FROM "timestamp") * 1000;--> statement-breakpoint
 ALTER TABLE "l1_l2_validator_proposer" ALTER COLUMN "timestamp" SET DEFAULT EXTRACT(EPOCH FROM NOW()) * 1000;--> statement-breakpoint
