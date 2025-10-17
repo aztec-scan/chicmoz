f142cb6dfb7c5d0872e912abb6ae6b97feb69402
WietzeS <wietze@kryha.io>
2025-10-17 14:59:15 +0200

    fix migration

--- services/explorer-api/migrations/0010_jittery_forgotten_one.sql
+++ services/explorer-api/migrations/0010_jittery_forgotten_one.sql
@@ -2,6 +2,6 @@ DROP TABLE "l1_l2_validator_proposer";--> statement-breakpoint
 DROP TABLE "l1_l2_validator_rollup_address";--> statement-breakpoint
 DROP TABLE "l1_l2_validator_stake";--> statement-breakpoint
 DROP TABLE "l1_l2_validator_status";--> statement-breakpoint
-DROP TABLE "l1_l2_validator";--> statement-breakpoint
 DROP TABLE "l1_l2_validator_withdrawer";--> statement-breakpoint
+DROP TABLE "l1_l2_validator";--> statement-breakpoint
 DROP TABLE "l2_chain_info";
