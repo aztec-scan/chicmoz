1f8ac761951d5611da6c2b3dae0bf62b04af3eaf
filip <filip.harald@gmail.com>
2026-02-20 11:58:07 +0100

    fix: backfill sponge blob hash in migration

--- services/explorer-api/migrations/0009_luxuriant_tomas.sql
+++ services/explorer-api/migrations/0009_luxuriant_tomas.sql
@@ -7,4 +7,8 @@ ALTER TABLE "l1_l2_validator_stake" ALTER COLUMN "timestamp" SET DEFAULT EXTRACT
 ALTER TABLE "l1_l2_validator_status" ALTER COLUMN "timestamp" SET DEFAULT EXTRACT(EPOCH FROM NOW()) * 1000;--> statement-breakpoint
 ALTER TABLE "l1_l2_validator_withdrawer" ALTER COLUMN "timestamp" SET DEFAULT EXTRACT(EPOCH FROM NOW()) * 1000;--> statement-breakpoint
 ALTER TABLE "l2BlockFinalizationStatus" ALTER COLUMN "timestamp" SET DEFAULT EXTRACT(EPOCH FROM NOW()) * 1000;--> statement-breakpoint
-ALTER TABLE "header" ADD COLUMN "sponge_blob_hash" varchar(66) NOT NULL;
+ALTER TABLE "header" ADD COLUMN "sponge_blob_hash" varchar(66);
+UPDATE "header"
+SET "sponge_blob_hash" = '0x0000000000000000000000000000000000000000000000000000000000000000'
+WHERE "sponge_blob_hash" IS NULL;
+ALTER TABLE "header" ALTER COLUMN "sponge_blob_hash" SET NOT NULL;
